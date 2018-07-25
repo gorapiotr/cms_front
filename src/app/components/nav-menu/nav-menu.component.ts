361cc204ac0d24e7209b5c084526bd0bea07eb43
gorapiotr <pgora@sigma.ug.edu.pl>
2018-07-25 21:22:27 +0200

    Logout and create before-login, after-login services(canActive for app routing)

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {TokenService} from '../../services/token.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
@@ -11,11 +13,23 @@ export class NavMenuComponent implements OnInit {
   public loggedIn: boolean;
 
   constructor(
-      private Auth: AuthService
+      private Auth: AuthService,
+      private router: Router,
+      private Token: TokenService
   ) { }
 
   ngOnInit() {
     this.Auth.authStatus.subscribe(value => this.loggedIn = value);
+  }
+
+  logout(event: MouseEvent) {
+    event.preventDefault();
+
+    this.Auth.changeAuthStatus(false);
+
+    this.Token.remove();
+
+    this.router.navigateByUrl('/login');
   }
 
 }
