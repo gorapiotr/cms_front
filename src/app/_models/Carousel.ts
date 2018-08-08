80802d005872619ea9d47f332b52b577bf471bcf
gorapiotr <pgora@sigma.ug.edu.pl>
2018-08-08 22:44:24 +0200

    Working at carousel component layout. Created Carousel model.

---
+++ src/app/_models/Carousel.ts
@@ -0,0 +1,26 @@
+export class Carousel {
+    id: number | undefined;
+    name: string;
+    alt: string;
+    position: number | undefined;
+    author: number | undefined;
+    active: boolean;
+
+
+    // constructor() {
+    //     this.id = undefined;
+    //     this.name = undefined;
+    //     this.alt = undefined;
+    //     this.position = undefined;
+    //     this.author = undefined;
+    // }
+
+    constructor(id, name, alt, position, author, active) {
+        this.id = id;
+        this.name = name;
+        this.alt = alt;
+        this.position = position;
+        this.author = author;
+        this.active = active;
+    }
+}
--- src/app/admin-panel/components/carousel/carousel.component.ts
+++ src/app/admin-panel/components/carousel/carousel.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import 'jquery-ui/ui/widgets/sortable.js';
+import {Carousel} from '../../../_models/Carousel';
+declare var $: any;
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements OnInit, AfterViewInit {
+
+     carousel: Array<Carousel> =[
+         new Carousel(0,"name", "alt", 1, 4, true),
+         new Carousel(1,"name", "alt", 2, 3, false),
+         new Carousel(2,"name", "alt", 3, 11, true),
+         new Carousel(3,"name", "alt", 4, 11, true),
+         new Carousel(4,"name", "alt", 2, 3, true),
+         new Carousel(5,"name", "alt", 3, 4, true)
+     ];
+
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+      $( '#sortable' ).sortable({
+          axis: 'y'
+      });
   }
 
 }
