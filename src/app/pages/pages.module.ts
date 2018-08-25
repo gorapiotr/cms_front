6a191b341c7ae0508962fee48d8999ab00e9a328
gorapiotr <pgora@sigma.ug.edu.pl>
2018-08-25 20:54:58 +0200

    Added component loader to shared.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {PagesComponent} from './pages.component';
 import {PagesRoutingModule} from './pages-routing.module';
+import {LoaderModule} from '../shared/loader/loader.module';
 
 @NgModule({
   imports: [
     RouterModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+      LoaderModule
   ],
   declarations: [PagesComponent]
 })
