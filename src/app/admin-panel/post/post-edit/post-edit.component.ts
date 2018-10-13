476df64337fb2ecff7c3fb78a643c050726a4280
gorapiotr <pgora@sigma.ug.edu.pl>
2018-10-13 22:14:02 +0200

    Working on post component.

--- src/app/admin-panel/post/post-edit/post-edit.component.ts
+++ src/app/admin-panel/post/post-edit/post-edit.component.ts
@@ -1,36 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {PostService} from '../../../_services/post/post.service';
 import {Post} from '../../../_models/Post/Post';
 
 @Component({
-  selector: 'app-post-edit',
-  templateUrl: './post-edit.component.html',
-  styleUrls: ['./post-edit.component.css']
+    selector: 'app-post-edit',
+    templateUrl: './post-edit.component.html',
+    styleUrls: ['./post-edit.component.css']
 })
 export class PostEditComponent implements OnInit {
 
     hideLoader = false;
     model: Post;
+    hideUpdateUserLoader = true;
 
-  constructor(
-      protected route: ActivatedRoute,
-      protected postService: PostService
-  ) { }
+    constructor(protected route: ActivatedRoute,
+                protected postService: PostService) {
+    }
 
-  ngOnInit() {
-    this.getPost();
-  }
+    ngOnInit() {
+        this.getPost();
+    }
 
-  getPost(){
-      this.hideLoader = true;
-      this.route.params.subscribe((params: Params) => {
-        this.postService.get(params.postId).subscribe( (data) => {
-          this.model = data.data;
-          this.hideLoader = true;
+    getPost() {
+        this.hideLoader = false;
+        this.route.params.subscribe((params: Params) => {
+            this.postService.get(params.postId).subscribe((data) => {
+                this.model = data.data;
+                console.log(this.model);
+                this.hideLoader = true;
+            });
         });
-      });
-  }
+    }
+
+    update() {
+        this.hideUpdateUserLoader = false;
+        // this.userService.update(this.user).subscribe((data) => {
+        //     this.getPost();
+        //     this.hideUpdateUserLoader = true;
+        // });
+    }
+
+    leadOptions: Object = {
+        charCounterCount: true,
+        toolbarButtons: [],
+        toolbarButtonsXS: [],
+        toolbarButtonsSM: [],
+        toolbarButtonsMD: [],
+    };
 }
 
 
