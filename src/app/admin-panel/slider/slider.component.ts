950c1f0ca90fa86a8abf6bca7ec72be7ff5ea30c
maciejSobczak <sobczakm161@gmail.com>
2018-10-29 23:29:28 +0100

    slider component from ng-bootstrap

--- src/app/admin-panel/slider/slider.component.ts
+++ src/app/admin-panel/slider/slider.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -9,18 +8,18 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
     providers: [NgbCarouselConfig]
 })
 
-export class SliderComponent implements OnInit {
-
+export class SliderComponent {
+    images = [1, 2, 3, 4].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
     showNavigationArrows = false;
     showNavigationIndicators = false;
-    images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
-    constructor(config: NgbCarouselConfig, private _http: HttpClient) {
+    constructor(config: NgbCarouselConfig) {
+        // customize default values of carousels used by this component tree
+        config.interval = 5000;
+        config.wrap = false;
+        config.keyboard = false;
+        config.pauseOnHover = true;
         config.showNavigationArrows = true;
         config.showNavigationIndicators = true;
     }
-
-    ngOnInit() {
-    }
-
 }
--- src/app/admin-panel/slider/slider.module.ts
+++ src/app/admin-panel/slider/slider.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SliderComponent } from './slider.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [{
+    path: '',
+    component: SliderComponent
+}];
 
 @NgModule({
-  imports: [
-    CommonModule
+    imports: [
+        CommonModule,
+        NgbModule.forRoot(),
+        RouterModule.forChild(routes)
   ],
-  declarations: []
+    declarations: [
+      SliderComponent
+  ],
+    exports: [SliderComponent]
 })
 export class SliderModule { }
