625e6add1254e287270846695e62aa6be0b428fb
maciejSobczak <sobczakm161@gmail.com>
2018-12-08 08:23:15 +0100

    process image before sending to storage

--- src/app/admin-panel/slider/slider.component.ts
+++ src/app/admin-panel/slider/slider.component.ts
@@ -5,9 +5,6 @@ import { SnotifyService} from "ng-snotify";
 import { SliderService } from '../../_services/slider/slider.service';
 import { HttpClient } from "@angular/common/http";
 import * as firebase from "firebase";
-import index from "@angular/cli/lib/cli";
-// import * as Rx from 'rxjs'
-// import 'rxjs/add/dom'
 
 @Component({
     selector: 'app-slider',
@@ -17,11 +14,9 @@ import index from "@angular/cli/lib/cli";
 })
 
 export class SliderComponent {
-    // exampleImages = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
     images: Array<string> = [];    //list of URLs for images stored in firebase storage
     canvas: HTMLCanvasElement;
     context;
-    // imgInCanvas = false;
     exampleImgPresented = false;
     showNavigationArrows = true;
     showNavigationIndicators = true;
@@ -56,7 +51,6 @@ export class SliderComponent {
         this.sliderService.get().subscribe(
             (data) => {
                 //get images from server, show in carousel, hide canvas
-                // if(data.length > 0) {
                     this.Notify.success("got " + data.length + " images");
                     if(this.exampleImgPresented == true) {
                         this.images.length = 0;
@@ -66,19 +60,13 @@ export class SliderComponent {
                     data.forEach((slide) => {
                         imagesList.push(slide.image_url);
                     });
-                    // this.imgInCanvas = false;
                 if (this.context) {
                     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
                 }
                     this.images = imagesList;
-                // } else {
-                //     this.exampleImgPresented = true;
-                //     this.Notify.success("no images -> display example");
-                //     this.images = this.exampleImages;
-                // }
             },
             (error) => {
-                console.log("failed to reach database");
+                this.Notify.error("database unreachable");
                 console.log(error);
             });
     }
@@ -96,43 +84,78 @@ export class SliderComponent {
 
     onUpload() {
         let uploader = <HTMLInputElement>document.getElementById('uploader');
-        console.log("lets see our image object");
-        console.log(this.selectedFile);
-        this.slide.file_name = this.selectedFile.name;
         let storageRef = firebase.storage().ref('postImages/' + this.selectedFile.name);
-        let task = storageRef.put(this.selectedFile);
-
-        task.on(firebase.storage.TaskEvent.STATE_CHANGED,
-            (snapshot: firebase.storage.UploadTaskSnapshot) => {
-                uploader.value = String((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-            },
-            (error) => {
-                console.log("firebase upload failed");
-                console.log(error);
-            },
-            () => {
-                console.log("img in firebase");
-                this.Notify.success("Image in firebase storage");
-                console.log(this.selectedFile);
-                storageRef.getDownloadURL().then((url) => {
-                    this.slide.image_url = url;
-                    this.uploadToDB();
-                    console.log("got url! uploading to db");
-                    console.log(this.slide.image_url);
-                },(error) => {
-                    console.log("coudn't get URL");
+        this.slide.file_name = this.selectedFile.name;
+        this.canvas.toBlob((blob) => {
+            let task = storageRef.put(blob);
+            task.on(firebase.storage.TaskEvent.STATE_CHANGED,
+                (snapshot: firebase.storage.UploadTaskSnapshot) => {
+                    uploader.value = String((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                },
+                (error) => {
+                    console.log(error);
+                    this.Notify.error("firebase upload failed");
+                },
+                () => {
+                    this.Notify.success("Image in firebase storage");
+                    console.log(this.selectedFile);
+                    storageRef.getDownloadURL().then((url) => {
+                        this.slide.image_url = url;
+                        this.uploadToDB();
+                    },(error) => {
+                        console.log("coudn't get URL");
+                        console.log(error);
+                    });
                 });
-            });
+        });
     }
 
+    /* load image pointed by param_file to canvas */
+    loadImage(event) {
+        this.selectedFile = <HTMLImageElement>event.target.files[0];
+        this.canvas = <HTMLCanvasElement>document.getElementById("canvas");
+        this.context = this.canvas.getContext("2d");
 
+// load file into canvas
+        var img = new Image();
+        img.src = URL.createObjectURL(this.selectedFile);
+        img.onload = () => {
+            var width = img.width;
+            var height = img.height;
+            this.canvas.width = width;
+            this.canvas.height = height;
+            this.context.drawImage(img,0,0);
+// replace transparent with white
+            var imageData;
+            try
+            {
+                imageData = this.context.getImageData(0, 0, width, height);
+            } catch(error)
+            {
+                console.log('getImageData from context failed');
+                console.log(error);
+            }
+            for (var i=0; i<height; i++)
+            {
+                for (var j=0; j<width; j++)
+                {
+                    let index = (i*width+j)*4;
+                    if (imageData.data[index+3] == 0)
+                    {
+                        imageData.data[index+3] = 255;
+                        imageData.data[index] = 255;
+                        imageData.data[index+1] = 255;
+                        imageData.data[index+2] = 255;
+                    }
+                }
+            }
+            this.context.putImageData(imageData, 0, 0);
+        };
+    }
 
     /* make canvas negative */
     negatyw() {
         var url = URL.createObjectURL(this.selectedFile);
-        console.log('URL');
-        console.log(url);
-        // var img = this.selectedFile; ???
         var img = new Image();
         img.onload = () => {
             this.context.drawImage(img, 900, 500);
@@ -150,8 +173,6 @@ export class SliderComponent {
             imageData = this.context.getImageData(0, 0, width, height) as HTMLImageElement;
         } catch(e)
         {
-            // netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
-            // imageData = this.context.getImageData(0, 0, width, height);
             this.Notify.error('no image loaded');
             console.log("no image to operate on");
             console.log(e);
@@ -159,101 +180,175 @@ export class SliderComponent {
 
 // make nagative
         for (var i=0; i<height; i++)
-        for (var j = 0; j < width; j++) {
-            var index = (i * width + j) * 4;
+            for (var j = 0; j < width; j++) {
+                var index = (i * width + j) * 4;
 // invert pixel
-            imageData.data[index + 3] = 255;
-            imageData.data[index] = 255 - imageData.data[index];
-            imageData.data[index + 1] = 255 - imageData.data[index + 1];
-            imageData.data[index + 2] = 255 - imageData.data[index + 2];
-        }
+                imageData.data[index + 3] = 255;
+                imageData.data[index] = 255 - imageData.data[index];
+                imageData.data[index + 1] = 255 - imageData.data[index + 1];
+                imageData.data[index + 2] = 255 - imageData.data[index + 2];
+            }
 
 // copy the image data back onto the canvas
         this.context.putImageData(imageData, 0, 0);
-        // imageData.name = this.selectedFile.name;
-        // this.selectedFile = imageData;
-        // console.log("imageData");
-        // console.log(imageData);
-
-        // let image = new Image();
-        // image.src = this.canvas.toDataURL();
-        // image.name = this.selectedFile.name;
-        // this.selectedFile = image;
-
-        // this.canvas.toBlob((blob) => {
-        //     let newImg = document.createElement('img'),
-        //         url = URL.createObjectURL(blob);
-        //
-        //     newImg.onload = function() {
-        //         // no longer need to read the blob so it's revoked
-        //         URL.revokeObjectURL(url);
-        //     };
-        //
-        //     newImg.src = url;
-        //     document.body.appendChild(newImg);
-        // });
-
-        console.log("selectedfile after toDataURL");
-        console.log(this.selectedFile);
     }
 
-    /* load image pointed by param_file to canvas */
-    loadImage(event) {
-        // this.imgInCanvas = true;    //Loaded image visible on page
-        this.selectedFile = <HTMLImageElement>event.target.files[0];
-        this.canvas = <HTMLCanvasElement>document.getElementById("canvas");
-        this.context = this.canvas.getContext("2d");
-        console.log("loadImage context");
-        console.log(this.context);
-
-// load file into canvas
+    /* Convert image loaded into canvas to sepia */
+    sepia(sepiaValue) {
+        var url = URL.createObjectURL(this.selectedFile);
         var img = new Image();
-        img.src = URL.createObjectURL(this.selectedFile);
-        console.log("onload selectedFile URL");
-        console.log(img.src);
         img.onload = () => {
-            var width = img.width;
-            var height = img.height;
-            this.canvas.width = width;
-            this.canvas.height = height;
-            this.context.drawImage(img,0,0);
-// replace transparent with white
-            var imageData;
-            try
-            {
-                imageData = this.context.getImageData(0, 0, width, height);
-            } catch(e)
-            {
-                console.log('getImageData from context failed');
-            }
-            for (var i=0; i<height; i++)
-            {
-                for (var j=0; j<width; j++)
-                {
-                    let index = (i*width+j)*4;
-                    if (imageData.data[index+3] == 0)
-                    {
-                        imageData.data[index+3] = 255;
-                        imageData.data[index] = 255;
-                        imageData.data[index+1] = 255;
-                        imageData.data[index+2] = 255;
-                    }
-                }
-            }
-            this.context.putImageData(imageData, 0, 0);
-            console.log("imgData");
-            console.log(imageData);
+            this.context.drawImage(img, 900, 500);
+        };
+        img.src = url;
 
-            console.log("selectedfile onload");
-            console.log(this.selectedFile);
+// read the width and height of the canvas
+        var width = this.canvas.width;
+        var height = this.canvas.height;
+
+// create a new pixel array
+        var imageData;
+        try
+        {
+            imageData = this.context.getImageData(0, 0, width, height) as HTMLImageElement;
+        } catch(e)
+        {
+            this.Notify.error('no image loaded');
+            console.log("no image to operate on");
+            console.log(e);
+        }
+        let index;
+        let val;
+    // convert to sepia
+        var newImageData = this.context.createImageData(width, height);
+        for (var i=0; i<height; i++)
+        {
+            for (var j=0; j<width; j++)
+            {
+                index = (i*width+j)*4;
+                val = 0.299*imageData.data[index+0] + 0.587*imageData.data[index+1] + 0.114*imageData.data[index+2];
+                if (val + 2 * sepiaValue > 255)
+                {
+                    newImageData.data[index+0] = 255;
+                }
+                else
+                {
+                    newImageData.data[index+0] = val + 2 * sepiaValue;
+                }
+                if (val + sepiaValue > 255)
+                {
+                    newImageData.data[index+1] = 255;
+                }
+                else
+                {
+                    newImageData.data[index+1] = val + sepiaValue;
+                }
+                newImageData.data[index+2] = val;
+                newImageData.data[index+3] = 255;
+            }
         }
 
-        console.log("selectedfile before negative");
-        console.log(this.selectedFile);
+// copy the image data back onto the canvas
+        this.context.putImageData(newImageData, 0, 0);
     }
 
-    handleError(err) {
-        this.error = err.error;
-        this.Notify.error(err);
+    /* make solarization on loaded image */
+    solarization() {
+        let luminanceValue = Number((document.getElementById("param_luminancja") as HTMLInputElement).value);
+
+        // var url = URL.createObjectURL(this.selectedFile);
+        var img = new Image();
+        img.onload = () => {
+            this.context.drawImage(img, 900, 500);
+        };
+        // img.src = url;
+
+// read the width and height of the canvas
+        var width = this.canvas.width;
+        var height = this.canvas.height;
+
+// create a new pixel array
+        var imageData;
+        try
+        {
+            imageData = this.context.getImageData(0, 0, width, height) as HTMLImageElement;
+        } catch(e)
+        {
+            this.Notify.error('no image loaded');
+            console.log("no image to operate on");
+            console.log(e);
+        }
+
+// prepare LUT table
+        let LUT: Array<number> = [];
+        for (var i=0; i<luminanceValue; i++)
+        {
+            LUT[i] = i;
+        }
+        for (var i=luminanceValue; i<256; i++)
+        {
+            LUT[i] = 255-i;
+        }
+// show LUT table
+        this.showLUT(LUT);
+
+        let index;
+// make solarization
+        var newImageData = this.context.createImageData(width, height);
+        for (var i=0; i<height; i++)
+        {
+            for (var j=0; j<width; j++)
+            {
+                index = (i*width+j)*4;
+                newImageData.data[index+3] = 255;
+                newImageData.data[index+0] = LUT[imageData.data[index+0]];
+                newImageData.data[index+1] = LUT[imageData.data[index+1]];
+                newImageData.data[index+2] = LUT[imageData.data[index+2]];
+            }
+        }
+
+// copy the image data back onto the canvas
+        this.context.putImageData(newImageData, 0, 0);
+    }
+
+    /* draw chart with LUT values */
+    showLUT(LUT) {
+        var lutCanvas = <HTMLCanvasElement>document.getElementById("lut_canvas");
+// clear previous
+        lutCanvas.width = lutCanvas.width;
+        var lutCtx = lutCanvas.getContext("2d");
+// draw texts
+        lutCtx.font = "10pt Arial";
+        lutCtx.fillStyle = "black";
+        lutCtx.textBaseline = "top";
+        lutCtx.textAlign = "center";
+        lutCtx.fillText("LUT", 65, 10);
+        lutCtx.font = "8pt Arial";
+        lutCtx.fillText("0", 8, 120);
+        lutCtx.fillText("255", 8, 0);
+        lutCtx.fillText("0", 25, 140);
+        lutCtx.fillText("255", 135, 140);
+// draw vertical arrow
+        lutCtx.moveTo(20, 130);
+        lutCtx.lineTo(20, 0);
+        lutCtx.lineTo(17, 5);
+        lutCtx.lineTo(20, 0);
+        lutCtx.lineTo(23, 5);
+        lutCtx.stroke();
+        lutCtx.moveTo(20, 130);
+// draw horizontal arrow
+        lutCtx.moveTo(20, 130);
+        lutCtx.lineTo(150, 130);
+        lutCtx.lineTo(145, 133);
+        lutCtx.lineTo(150, 130);
+        lutCtx.lineTo(145, 127);
+        lutCtx.stroke();
+// draw chart
+        lutCtx.moveTo(20, 130);
+        for (var i=0; i<256; i++)
+        {
+            lutCtx.lineTo(20+i/2, 130-LUT[i]/2);
+        }
+        lutCtx.stroke();
     }
 }
